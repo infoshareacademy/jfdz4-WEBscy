8205e9e73981075c99758f90f329b602c2e841fa
Adam Grabarczyk <adamgrabarczyk.gda@wp.pl>
2017-05-04 00:19:54 +0200

    add validation

--- js/game.js
+++ js/game.js
@@ -33,14 +33,32 @@ function showBoard() {
     });
 }
 
+$('#button').unbind('click').bind('click', function () {
+    var form = document.forms["myForm"]["email"].value;
+    var monkey = form.indexOf("@");
+    var dot = form.lastIndexOf(".");
+    if (monkey<1 || dot<monkey+2 || dot+2>=form.length) {
+        alert("Wprowadź poprawny adres e-mail");
+        $('.placeholder').val('');
+        return false;
+    }
+    if (!jQuery("#checkbox").is(":checked")) {
+        alert("Musisz wyrazić zgode aby przejść dalej");
+        return false;
 
-$(function() {
-    $('#button').click(function() {
+    }
+
+    else
+    {
         $('#game').toggle(800).toggleClass('show');
         $('#instruction').toggle(600).toggleClass('show');
-        $('#zapiszsie').toggle(400).toggleClass('hide');
-
-    });
+        $('#zapiszsie').toggle(400).toggleClass('hide');;
+        setTimeout(function() {
+            $('.placeholder').val('');
+        }, 1);
+        return false;
+    }
+});
 
         showBoard();
 
@@ -54,6 +72,11 @@ $(function() {
             if (
                 $(this).hasClass('event')
 
-            ) {}
+            ) {
+            }
 
-        }); })
+            // });
+
+            // })
+
+    })
