0705e3a691bb7815e7a16ea02f58adfbf1d30b60
Adam Grabarczyk <adamgrabarczyk.gda@wp.pl>
2017-05-11 19:35:41 +0200

    remove Adam's www

--- about_me/Adam/form.js
+++
@@ -1,6 +0,0 @@
-$('#button').click(function (event) {
-    event.preventDefault();
-    $('#moral').toggle(800).toggleClass('show');
-    // $('#instruction').toggle(600).toggleClass('show');
-    $('.contact').toggle(400).toggleClass('hide');
-})
